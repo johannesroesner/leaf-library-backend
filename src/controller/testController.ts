9c9a69924c775c1b9e24067d2b22797f8f34b362
johannesroesner <roesnerj@icloud.com>
2025-11-02 11:47:40 +0100

    fix: eslient

--- src/controller/testController.ts
+++ src/controller/testController.ts
@@ -1,9 +1,7 @@
 import type { Request, ResponseToolkit } from "@hapi/hapi";
 
 export const testController = {
-  index: (request: Request, h: ResponseToolkit) => {
-    return h.view("index-view", {
-      title: "Leaf Library",
-    });
-  },
+  index: (request: Request, h: ResponseToolkit) => h.view("index-view", {
+    title: "Leaf Library",
+  }),
 };
